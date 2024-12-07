d22a33d15019d5fd24287e4d8ecacbbb7f9b0302
tobyStupp <40300257+tobyStupp@users.noreply.github.com>
2024-12-08 01:27:52 +0200

    Add project files.

---
+++ QueuePractice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueuePractice
+{
+    class Program
+    {
+        static int Count(Queue<int> q)
+        {
+            throw new NotImplementedException();
+        }
+        static int Sum (Queue<int> q)
+        {
+            throw new NotImplementedException();
+        }
+        static bool Found (Queue<int> q, int value)
+        {
+            throw new NotImplementedException();
+        }
+        static void RemoveDuplicates(Queue<int> q) => throw new NotImplementedException();
+        static bool IsSorted(Queue<int> q) => throw new NotImplementedException();
+        static void AddSorted(Queue<int> q, int x) => throw new NotImplementedException();
+        static Queue<int> Intersect(Queue<int> q1, Queue<int> q2) => throw new NotImplementedException();
+
+    static Queue<int> Copy (Queue<int> q)
+        static void Main(string[] args)
+        {
+        }
+    }
+}
